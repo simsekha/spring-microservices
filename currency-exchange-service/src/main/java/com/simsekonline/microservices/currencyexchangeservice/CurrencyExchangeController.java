83d13469e63165a16e1172ab25a8abbab97fcd30
System Administrator <root@Hayri-MacBook-Pro.local>
2018-12-07 22:38:42 +0300

    JPA is OK

--- currency-exchange-service/src/main/java/com/simsekonline/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/simsekonline/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -2,8 +2,8 @@ package com.simsekonline.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
-import org.omg.CORBA.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,15 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 
-	//@Autowired
-	//private Environment environment;
+    @Autowired
+    private Environment environment;
 
     @GetMapping("/currency-exchange-service/from/{from}/to/{to}")
-    public ExchangeValue retreiveExchangeValue(@PathVariable String from, @PathVariable String to) {    
+    public ExchangeValue retreiveExchangeValue(@PathVariable String from, @PathVariable String to) {
         ExchangeValue exchangeValue = new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
-        //exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
-        exchangeValue.setPort(8000);
-
+        exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
         return exchangeValue;
     }
 }
