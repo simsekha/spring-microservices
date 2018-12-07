e89e26938e56047ddb0432b87a0b3bd8a3b4039e
hayri.simsek <simsekha@hotmail.com>
2018-12-07 22:50:21 +0300

    DATA Fetched with JPA

--- currency-exchange-service/src/main/java/com/simsekonline/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/simsekonline/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,7 +1,5 @@
 package com.simsekonline.microservices.currencyexchangeservice;
 
-import java.math.BigDecimal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,9 +12,12 @@ public class CurrencyExchangeController {
     @Autowired
     private Environment environment;
 
+    @Autowired
+    private ExchangeValueRepository exchangeValueRepository;
+    
     @GetMapping("/currency-exchange-service/from/{from}/to/{to}")
-    public ExchangeValue retreiveExchangeValue(@PathVariable String from, @PathVariable String to) {
-        ExchangeValue exchangeValue = new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+    public ExchangeValue retreiveExchangeValue(@PathVariable String from, @PathVariable String to) {    	
+        ExchangeValue exchangeValue = exchangeValueRepository.findByFromAndTo(from, to);
         exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
         return exchangeValue;
     }
